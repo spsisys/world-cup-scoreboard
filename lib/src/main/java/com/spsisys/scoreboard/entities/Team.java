3a7d9d4e5c78067a288396a33f34f4cfcccd67a7
SPSI <spsisys@gmail.com>
2023-05-26 09:12:19 +0200

    simple entity Team

--- lib/src/main/java/com/spsisys/scoreboard/entities/Team.java
+++ lib/src/main/java/com/spsisys/scoreboard/entities/Team.java
@@ -1,4 +1,32 @@
 package com.spsisys.scoreboard.entities;
 
 public class Team {
+    Integer id;
+    String name;
+
+    public Team(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // TODO: default constructor
+    public Team() {
+    }
+
+    // getters and setters
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
