527d922e89ab7d21d13bb2abea79a7bb2b275570
SPSI <spsisys@gmail.com>
2023-05-27 11:09:39 +0200

    create TrialApp for testing Library

---
+++ lib/src/main/java/com/spsisys/scoreboard/TrialApp.java
@@ -0,0 +1,36 @@
+package com.spsisys.scoreboard;
+
+import com.spsisys.scoreboard.entities.Game;
+import com.spsisys.scoreboard.exceptions.TeamIsPlayingException;
+import com.spsisys.scoreboard.stores.TrialAppData;
+
+public class TrialApp {
+    public static void main(String[] args) throws InterruptedException {
+        Library library = new Library();
+
+        Game game1;
+        Game game2;
+        Game game3;
+        Game game4;
+        Game game5;
+
+        try {
+            game1 = library.startGame(TrialAppData.MEXICO, TrialAppData.CANADA);
+            game2 = library.startGame(TrialAppData.SPAIN, TrialAppData.BRAZIL);
+            game3 = library.startGame(TrialAppData.GERMANY, TrialAppData.FRANCE);
+            game4 = library.startGame(TrialAppData.URUGUAY, TrialAppData.ITALY);
+            game5 = library.startGame(TrialAppData.ARGENTINA, TrialAppData.AUSTRALIA);
+
+            library.updateScore(game1, 0, 5);
+            library.updateScore(game2, 0, 5);
+            library.updateScore(game3, 0, 5);
+            library.updateScore(game4, 0, 5);
+            library.updateScore(game5, 0, 5);
+        } catch (TeamIsPlayingException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("******** SUMMARY OF CURRENT GAMES BY TOTAL SCORE ********\n" + library.getSummaryAsString());
+        }
+    }
+}
---
+++ lib/src/main/java/com/spsisys/scoreboard/stores/TrialAppData.java
@@ -0,0 +1,16 @@
+package com.spsisys.scoreboard.stores;
+
+import com.spsisys.scoreboard.entities.Team;
+
+public class TrialAppData {
+    public static final Team MEXICO = new Team(1, "Mexico");
+    public static final Team CANADA = new Team(2, "Canada");
+    public static final Team SPAIN = new Team(3, "Spain");
+    public static final Team BRAZIL = new Team(4, "Brazil");
+    public static final Team GERMANY = new Team(5, "Germany");
+    public static final Team FRANCE = new Team(6, "France");
+    public static final Team URUGUAY = new Team(7, "Uruguay");
+    public static final Team ITALY = new Team(8, "Italy");
+    public static final Team ARGENTINA = new Team(9, "Argentina");
+    public static final Team AUSTRALIA = new Team(10, "Australia");
+}
